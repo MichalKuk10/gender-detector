900626627e90737b4988ca58038c653125458681
michal <kuk.michal3@gmail.com>
2021-03-16 17:53:52 +0100

    add service changes

--- src/main/java/project/dao/DAO.java
+++ src/main/java/project/dao/DAO.java
@@ -1,7 +1,6 @@
 package project.dao;
 
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -10,8 +9,7 @@ import java.util.List;
 @Component
 public class DAO  {
 
-        public  List<String> fetchFemaleData() throws IOException {
-             File filepath = new File("/Users/michal/Desktop/Codecool/Projects/gender-detector/src/main/resources/female.txt");
+        public  List<String> fetchDataFromFile(File filepath) throws IOException {
 
             BufferedReader br = new BufferedReader(new FileReader(filepath));
             List<String> namesList =  new ArrayList<>();
--- src/main/java/project/service/GenderService.java
+++ src/main/java/project/service/GenderService.java
@@ -2,12 +2,17 @@ package project.service;
 
 import org.springframework.stereotype.Service;
 import project.dao.DAO;
+import project.model.Gender;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 @Service
 public class GenderService {
+   private final File filepathFemale = new File("/Users/michal/Desktop/Codecool/Projects/gender-detector/src/main/resources/female.txt");
+   private final File filepathMale = new File("/Users/michal/Desktop/Codecool/Projects/gender-detector/src/main/resources/male.txt");
+
 
     public DAO dao;
 
@@ -15,19 +20,34 @@ public class GenderService {
         this.dao = dao;
     }
 
-    public String checkGender(String name) throws IOException {
+    public Gender checkGender(String name) throws IOException {
 
         try {
-            List<String> stringList = dao.fetchFemaleData();
-            for (String element : stringList){
-                if (element.equals(name)){
-                    return "FEMALE";
-                }
+
+            if(fetchResult(filepathFemale, name)){
+                return Gender.FEMALE;
+            }else if(fetchResult(filepathMale, name)){
+                return Gender.MALE;
+            }else {
+                return Gender.INCONCLUSIVE;
             }
+
         }catch (IOException e){
             e.printStackTrace();
         }
-        return "MALE";
+
+        return null;
+    }
+
+    private boolean fetchResult(File filepath, String name) throws IOException {
+        List<String> stringList = dao.fetchDataFromFile(filepath);
+        for (String element : stringList){
+            if (element.equals(name)){
+                return true;
+            }
+        }
+        return false;
+
     }
 
 
